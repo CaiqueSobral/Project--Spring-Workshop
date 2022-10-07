1e61039a46dd82fd34a3dfe5e97b7b58679b28db
Caique Sobral <caiquelsobral@gmail.com>
2022-10-07 10:56:05 -0300

    Feat: implements category

---
+++ src/main/java/com/springws/course/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.springws.course.repositories;
+
+import com.springws.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
--- src/main/java/com/springws/course/resources/CategoryResource.java
+++ src/main/java/com/springws/course/resources/CategoryResource.java
@@ -1,7 +1,7 @@
 package com.springws.course.resources;
 
-import com.springws.course.entities.User;
-import com.springws.course.services.UserService;
+import com.springws.course.entities.Category;
+import com.springws.course.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,22 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 
     @Autowired
-    private UserService service;
+    private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<Category>> findAll(){
 
-        List<User> list = service.findAll();
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = service.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+        Category obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 }
