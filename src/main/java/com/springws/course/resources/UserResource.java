6add766a45f9617f189e9c914c145eb3ccef40f6
Caique Sobral <caiquelsobral@gmail.com>
2022-10-14 09:19:29 -0300

    FEAT: Error handling

--- src/main/java/com/springws/course/resources/UserResource.java
+++ src/main/java/com/springws/course/resources/UserResource.java
@@ -19,7 +19,6 @@ public class UserResource {
 
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-
         List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
